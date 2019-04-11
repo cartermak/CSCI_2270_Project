d6b745c479d943d7548f2fb39072e05c55eff2f4
wiwa5607 <wiwa5607@colorado.edu>
2019-04-10 19:42:49 -0600

    Reorganized Files

--- examples/proof of concept/time.cpp
+++ examples/proof of concept/time.cpp
@@ -2,16 +2,17 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include "project.hpp"
 using namespace std;
 
-
-int main() {
+int main()
+{
     MachinePart temp;
     auto start = std::chrono::system_clock::now();
     std::time_t end_time = std::chrono::system_clock::to_time_t(start);
     cout << ctime(&end_time);
-    temp.date = *localtime(&end_time);
-    end_time = mktime(&temp.date);
+    // temp.date = *localtime(&end_time);
+    // end_time = mktime(&temp.date);
     cout << ctime(&end_time);
     return 0;
 }
