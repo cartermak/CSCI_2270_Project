e161ff1b5660c500b2f7df18838b42212c66ce12
cartermak <cartermak3@gmail.com>
2019-04-15 11:49:25 -0600

    started the class

---
+++ strMod_class/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+class strMod
+{
+  public:
+    strMod()
+    {
+    }
+    strMod(int b)
+    {
+        b = a;
+        
+        // for (char &c : instr)
+        // {
+        //     str.push_back(c);
+        // }
+    }
+    ~strMod()
+    {
+    }
+    
+
+  private:
+    int a;
+};
+
+int main()
+{
+    strMod x(3);
+    
+    return 0;
+}
